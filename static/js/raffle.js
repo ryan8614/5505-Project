49255bf0bdcab2a136c20b7dbf085d283f509149
Shaohongzheng <colinzheng2329@gmail.com>
2024-05-10 23:51:44 +0800

    Updating the error message for following the same message of raffle function in routes.py

--- static/js/raffle.js
+++ static/js/raffle.js
@@ -1,19 +1,15 @@
 $(document).ready(function() {
     $('#raffleButton').click(function() {
         $.post('/raffle', function(data) {
-            if (data.error) {
-                // Update the text of the errorMessage span with the received error message
-                $('#errorMessage').text(data.error);
-                // Show the modal with the error message
-                $('#errorModal').modal('show');
+            $('#winningSplitImage').attr('src', data.fragment_path); 
+            $('#winningSplitName').text('Congratulations! You won: ' + data.fragment_name);
+            $('#raffleModal').modal('show');
+        }).fail(function(jqXHR, textStatus, errorThrown) {
+            if (jqXHR.responseJSON && jqXHR.responseJSON.error) {
+                $('#errorMessage').text(jqXHR.responseJSON.error);
             } else {
-                $('#winningSplitImage').attr('src', data.fragment_path); 
-                $('#winningSplitName').text('Congratulations! You won: ' + data.fragment_name);
-                $('#raffleModal').modal('show');
+                $('#errorMessage').text('Error: An unexpected error occurred. Please try again.');
             }
-        }).fail(function(response) {
-            // In case the AJAX request itself fails, we provide a generic error message
-            $('#errorMessage').text('Error: There are no more fragments.');
             $('#errorModal').modal('show');
         });
     });
