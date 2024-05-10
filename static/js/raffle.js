a6fed0e78370e929abe7357d5aa851ef5554b9ea
Shaohongzheng <colinzheng2329@gmail.com>
2024-05-10 22:47:35 +0800

    Adding a new Modal box for Error situation and also do some modification on error message

--- static/js/raffle.js
+++ static/js/raffle.js
@@ -2,14 +2,19 @@ $(document).ready(function() {
     $('#raffleButton').click(function() {
         $.post('/raffle', function(data) {
             if (data.error) {
-                alert(data.error);
+                // Update the text of the errorMessage span with the received error message
+                $('#errorMessage').text(data.error);
+                // Show the modal with the error message
+                $('#errorModal').modal('show');
             } else {
                 $('#winningSplitImage').attr('src', data.fragment_path); 
                 $('#winningSplitName').text('Congratulations! You won: ' + data.fragment_name);
                 $('#raffleModal').modal('show');
             }
         }).fail(function(response) {
-            alert('Error: ' + response.responseText);
+            // In case the AJAX request itself fails, we provide a generic error message
+            $('#errorMessage').text('Error: There are no more fragments.');
+            $('#errorModal').modal('show');
         });
     });
 });
